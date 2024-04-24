5aa49504e8960b980dd02e21a75ba07660ef32ca
[asdfwefsf] <bethel5308@naver.com>
2024-04-24 19:40:49 +0900

    Initial commit

---
+++ app/src/main/java/com/company/kodingtest/1676.kt
@@ -0,0 +1,5 @@
+package com.company.kodingtest
+
+fun main() {
+    
+}
---
+++ app/src/main/java/com/company/kodingtest/koding.kt
@@ -0,0 +1,30 @@
+package com.company.kodingtest
+
+//fun main() {
+//    val (k, n) = readLine()!!.split(" ").map {
+//        it.toInt()
+//    }
+//    val lans = Array(k) {
+//        readLine()!!.toLong()
+//    }
+//
+//    var left: Long = 1L
+//    var right: Long = lans.maxOrNull()!!
+//    var mid: Long
+//
+//    while (left <= right) {
+//        mid = (left + right) / 2
+//        var count : Long = 0
+//
+//        for (lan in lans) {
+//            count += lan / mid
+//        }
+//
+//        if (count >= n) {
+//            left = mid + 1
+//        } else {
+//            right = mid - 1
+//        }
+//    }
+//    println(right)
+//}
