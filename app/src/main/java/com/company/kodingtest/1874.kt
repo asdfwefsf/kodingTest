1367f86593ebaa318fae7a8b8af2f97824e1dc0e
[asdfwefsf] <bethel5308@naver.com>
2024-04-27 16:14:20 +0900

    1929

--- app/src/main/java/com/company/kodingtest/1874.kt
+++ app/src/main/java/com/company/kodingtest/1874.kt
@@ -7,28 +7,28 @@ import java.util.Stack
 // Stack 사용하고 싶으면 : import java.util.Stack
 // StringBuilder 사용하고 싶으면 : import java.util.Stack
 
-fun main() = with(System.`in`.bufferedReader()) {
-    val sb = StringBuilder()
-
-    val stack = Stack<Int>()
-
-    val inputArray = Array(readLine().toInt()) { readLine().toInt() }
-
-    var initial = 1
-
-    for (num in inputArray) {
-        while (initial <= num) {
-            stack.push(initial++)
-            sb.append("+\n")
-        }
-        if(stack.peek() == num) {
-            stack.pop()
-            sb.append("-\n")
-        } else {
-            println("NO")
-            return@with
-        }
-    }
-    println(sb)
-}
-
+//fun main() = with(System.`in`.bufferedReader()) {
+//    val sb = StringBuilder()
+//
+//    val stack = Stack<Int>()
+//
+//    val inputArray = Array(readLine().toInt()) { readLine().toInt() }
+//
+//    var initial = 1
+//
+//    for (num in inputArray) {
+//        while (initial <= num) {
+//            stack.push(initial++)
+//            sb.append("+\n")
+//        }
+//        if(stack.peek() == num) {
+//            stack.pop()
+//            sb.append("-\n")
+//        } else {
+//            println("NO")
+//            return@with
+//        }
+//    }
+//    println(sb)
+//}
+//
---
+++ app/src/main/java/com/company/kodingtest/1929.kt
@@ -0,0 +1,28 @@
+package com.company.kodingtest
+
+import java.lang.StringBuilder
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val bw = System.out.bufferedWriter()
+    val sb = StringBuilder()
+
+    val (first , last) = readLine().split(" ").map { it.toInt() }
+
+    val array = BooleanArray(last + 1) { false }
+    array[1] = true // 2도 소순데 배열 생성 할 때 초기 배열 설정 할 때 false로 설정되었으니까 수동으로 true로 설정해주기
+
+    for (i in 2..last) {
+        if (!array[i]) {
+            for (j in i * 2..last step i) {
+                array[j] = true
+            }
+        }
+    }
+
+    val primes = (first..last).filter { !array[it] }
+
+    primes.forEach { sb.append(it).append('\n') }
+    bw.write("$sb")
+    bw.flush()
+    bw.close()
+}
